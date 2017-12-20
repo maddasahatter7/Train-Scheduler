d406ac856334bcee8bad8258492137eab86508e4
Matt Barnett <Madison@Madisons-MacBook-Pro.local>
2017-12-19 22:50:38 -0500

    moment is driving me insane, have data appending to DOM but haven't figured out conversion for correct times

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,6 +18,7 @@
   var nextTrain;
   var minutesUntilTrain;
   var firstTimeConverted;
+  // moment current time method
   var currentTime = moment();
   console.log(currentTime);
   var diffTime;
@@ -33,20 +34,26 @@ $("#btn-submit").on("click", function(event) {
     destination = $("#dest").val().trim();
     frequency = $("#freq").val().trim();
     firstTrainTime = $("#fstTrn").val().trim();
+    // Intitial value for time, having errors trying to format correctly
     firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
     console.log(firstTimeConverted);
+    // Trying to take the current time and get the difference of the firstTimeConverted
     diffTime = currentTime.diff(moment(firstTimeConverted), "minutes");
     console.log(diffTime);
+    // Trying to get the difference between the times between train rides
     tRemainder = diffTime % frequency;
     console.log(tRemainder);
+      // Trying to get the difference between the times between train rides, still not working correctly
     minutesUntilTrain = frequency - tRemainder;
     console.log(minutesUntilTrain);
+    // Adding current time to next train
     nextTrain = moment().add(minutesUntilTrain, "minutes");
     console.log(nextTrain);
+    // Finally formatting the time for Next Arrival
     nextTrainFormat = moment(nextTrain).format("HH:mm");
     console.log(nextTrainFormat);
     
-    
+    // object that contains data that will enter firebase
     var train = {
         trainName: trainName,
         destination: destination,
@@ -55,13 +62,13 @@ $("#btn-submit").on("click", function(event) {
         nextTrainFormat: nextTrainFormat,
         minutesUntilTrain: minutesUntilTrain
     }
-
+     
      console.log(train);
 
     database.ref().push(train);
 
    
-
+    // yay we got one!
     alert("You've booked a train ride!");
     // Clear out input boxes after submit
     trainName = $("#trnName").val("");
@@ -71,17 +78,23 @@ $("#btn-submit").on("click", function(event) {
 
 });
 
-
+// At the initial load, get a snapshot of the current data.
 database.ref().on("child_added", function(snapshot){
-  var snap = snapshot.val();
-  console.log(snap);
+    // Need to know when first train arrives and how often 
+// Train Infomations :)
 
-  $("#trnName").text(snap.trainName);
-  console.log
-  $("#dest").text(snapshot.val().destination);
-  $("#freq").text(snapshot.val().frequency);
-  $("#fstTrn").text(snapshot.val().firstTrainTime);
+ var childDestination = snapshot.val().destination;
+ var childFrequency = snapshot.val().frequency;
+ var childTrainTime = snapshot.val().firstTrainTime;
+ var childTrainName = snapshot.val().trainName;
+ var childNextFormat = snapshot.val().nextTrainFormat;
+//  var childMinutesUntilTrain = snapshot.val().minutesUntilTrain;
 
-
-
+ console.log(childDestination, childFrequency, childTrainTime, childTrainName, childNextFormat);
+// grabbing last object/child from database and appending to the DOM
+ $("#table").append(
+    "<tr><td>" + childTrainName + "</td><td>" + childDestination 
+    + "</td><td>" + childFrequency + "</td><td>"
+    + childNextFormat + "</td><td>"
+    + childTrainTime + "</td></tr>");
 });
